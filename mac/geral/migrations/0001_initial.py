f0ce185c7afe6ebaf8be85c4d19b4fb33fba9a90
Filipa Andrade <filipa.andrade@gmail.com>
2021-03-28 20:23:16 +0200

    Add initial migrations for all apps

---
+++ mac/geral/migrations/0001_initial.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import filebrowser.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Destaque',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=50)),
+                ('imagem', models.ImageField(null=True, upload_to=b'inicial/', blank=True)),
+                ('texto', models.TextField(null=True, blank=True)),
+                ('texto_en', models.TextField(null=True, blank=True)),
+                ('url', models.CharField(max_length=100)),
+                ('visivel', models.BooleanField(default=False)),
+                ('ordem', models.SmallIntegerField()),
+            ],
+            options={
+                'ordering': ['nome'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Foto',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=100)),
+                ('thumbnail', filebrowser.fields.FileBrowseField(max_length=200, null=True, blank=True)),
+                ('image', filebrowser.fields.FileBrowseField(max_length=200, null=True, blank=True)),
+            ],
+            options={
+                'ordering': ['nome'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Material',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=50)),
+            ],
+            options={
+                'ordering': ['nome'],
+                'verbose_name': 'Material',
+                'verbose_name_plural': 'Materiais',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tecnica',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=50)),
+            ],
+            options={
+                'ordering': ['nome'],
+                'verbose_name': 'Tecnica',
+                'verbose_name_plural': 'Tecnicas',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tela',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titulo', models.CharField(max_length=200)),
+                ('image', models.ImageField(upload_to=b'geral/')),
+            ],
+            options={
+                'ordering': ['titulo'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Tipo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=50)),
+            ],
+            options={
+                'ordering': ['nome'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='tecnica',
+            name='categoria',
+            field=models.ForeignKey(to='geral.Tipo'),
+            preserve_default=True,
+        ),
+        migrations.AddField(
+            model_name='material',
+            name='categoria',
+            field=models.ForeignKey(to='geral.Tipo'),
+            preserve_default=True,
+        ),
+    ]
---
+++ mac/publicacoes/migrations/0001_initial.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('artistas', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Link',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('nome', models.CharField(max_length=200)),
+                ('url', models.URLField()),
+            ],
+            options={
+                'ordering': ['nome'],
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Publicacao',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titulo', models.CharField(max_length=200)),
+                ('autor', models.CharField(max_length=100, null=True, blank=True)),
+                ('editor', models.CharField(max_length=100, null=True, blank=True)),
+                ('tipo', models.CharField(max_length=10, choices=[('Newsletter', 'Newsletter'), ('Catalogo', 'Catalogo'), ('Monografia', 'Monografia'), ('Imprensa', 'Imprensa'), ('Critica', 'Critica')])),
+                ('data', models.DateField(null=True, blank=True)),
+                ('descricao', models.TextField(null=True, blank=True)),
+                ('descricao_en', models.TextField(null=True, blank=True)),
+                ('ficheiro', models.FileField(max_length=200, null=True, upload_to=b'publicacoes/', blank=True)),
+                ('artista', models.ForeignKey(blank=True, to='artistas.Artista', null=True)),
+            ],
+            options={
+                'ordering': ['-data'],
+                'verbose_name': 'Publicacao',
+                'verbose_name_plural': 'Publicacoes',
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Texto',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('titulo', models.CharField(max_length=500)),
+                ('texto', models.TextField(null=True, blank=True)),
+                ('texto_en', models.TextField(null=True, blank=True)),
+            ],
+            options={
+                'ordering': ['titulo'],
+            },
+            bases=(models.Model,),
+        ),
+    ]
