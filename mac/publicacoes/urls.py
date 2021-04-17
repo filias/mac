639f4a94579142472215e391a1a6582b58c7a2af
DanielaNy <daniela.nyblova@gmail.com>
2021-04-17 18:52:02 +0200

    Remove Newsletter and Monografia publications

--- mac/publicacoes/urls.py
+++ mac/publicacoes/urls.py
@@ -4,9 +4,7 @@ from .views import *
 
 urlpatterns = [
     url(r"^$", index),
-    url(r"^newsletters/(?P<select>\w+)/$", newsletters),
     url(r"^catalogos/(?P<select>\w+)/$", catalogos),
-    url(r"^monografias/(?P<select>\w+)/$", monografias),
     url(r"^imprensa/(?P<select>\w+)/$", imprensa),
     url(r"^critica/(?P<select>\w+)/$", critica),
 ]
--- mac/publicacoes/views.py
+++ mac/publicacoes/views.py
@@ -7,9 +7,7 @@ RTR_DICT = common.DEFAULT_DICT
 
 
 def index(request):
-    RTR_DICT["monografias"] = Publicacao.objects.filter(tipo="Monografia").order_by(
-        "-data"
-    )[:3]
+
     RTR_DICT["catalogos"] = Publicacao.objects.filter(tipo="Catalogo").order_by(
         "-data"
     )[:3]
@@ -22,29 +20,11 @@ def index(request):
     return render(request, "publicacoes/templates/publicacoes.html", RTR_DICT)
 
 
-def newsletters(request, select):
-    RTR_DICT["publicacoes"] = Publicacao.objects.filter(
-        tipo="Newsletter", artista__nome__range=(select[0], select[1])
-    ).order_by("artista")
-    return render(
-        request, "publicacoes/templates/publicacoes_newsletters.html", RTR_DICT
-    )
-
-
 def catalogos(request, select):
     RTR_DICT["publicacoes"] = Publicacao.objects.filter(
         tipo="Catalogo", artista__nome__range=(select[0], select[1])
     ).order_by("artista")
     return render(request, "publicacoes/templates/publicacoes_catalogos.html", RTR_DICT)
-
-
-def monografias(request, select):
-    RTR_DICT["publicacoes"] = Publicacao.objects.filter(
-        tipo="Monografia", artista__nome__range=(select[0], select[1])
-    ).order_by("artista")
-    return render(
-        request, "publicacoes/templates/publicacoes_monografias.html", RTR_DICT
-    )
 
 
 def imprensa(request, select):
