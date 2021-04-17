2377b5ed99dce2c122c74415daeb6ae01aa85a8a
Filipa Andrade <filipa.andrade@gmail.com>
2021-04-17 15:57:11 +0200

    Use url template tag in exhibitions app and name views

--- mac/exposicoes/urls.py
+++ mac/exposicoes/urls.py
@@ -3,11 +3,19 @@ from django.conf.urls import url
 from .views import *
 
 urlpatterns = [
-    url(r"^actuais/$", index),
-    url(r"^passadas/$", passadas),
-    url(r"^futuras/$", futuras),
-    url(r"^passadas/(?P<exposicao_ano>\d+)/$", passadas_ano),
-    url(r"^(?P<exposicao_id>\d+)/$", detail),
-    url(r"^(?P<exposicao_id>\d+)/obras/$", obras),
-    url(r"^(?P<exposicao_id>\d+)/obras/(?P<obra_id>\d+)/$", obra_detalhe),
+    url(r"^actuais/$", index, name="current_exhibitions"),
+    url(r"^passadas/$", passadas, name="past_exhibitions"),
+    url(r"^futuras/$", futuras, name="future_exhibitions"),
+    url(
+        r"^passadas/(?P<exposicao_ano>\d+)/$",
+        passadas_ano,
+        name="past_exhibitions_per_year",
+    ),
+    url(r"^(?P<exposicao_id>\d+)/$", detail, name="exhibition_detail"),
+    url(r"^(?P<exposicao_id>\d+)/obras/$", obras, name="exhibition_works"),
+    url(
+        r"^(?P<exposicao_id>\d+)/obras/(?P<obra_id>\d+)/$",
+        obra_detalhe,
+        name="work_detail",
+    ),
 ]
