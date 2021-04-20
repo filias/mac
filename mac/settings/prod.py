467e645b6257ab9ddc4b10a082ac90d1cc3c7278
Filipa Andrade <filipa.andrade@gmail.com>
2021-04-20 14:28:36 +0200

    Add email sending settings for production

--- mac/settings/prod.py
+++ mac/settings/prod.py
@@ -1,3 +1,10 @@
 from .base import *
 
 ALLOWED_HOSTS = ["macsiteadmin.pythonanywhere.com", "movimentoartecontemporanea.com"]
+
+# Email sending - gmail
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "webmaster@movimentoartecontemporanea.com"
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
