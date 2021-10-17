08fde66cdbca57acf47b2bd0adb0ba46ea556bcf
Filipa Andrade <fandrade@laterpay.net>
2021-10-17 16:28:31 +0200

    Use path in gallery urls

--- mac/gallery/urls.py
+++ mac/gallery/urls.py
@@ -1,30 +1,30 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import *
 
 urlpatterns = [
-    url(r"^$", index, name="gallery"),
-    url(r"^galerias$", galerias, name="galleries"),
-    url(r"^missao/$", missao, name="mission"),
-    url(r"^historia/$", historia, name="history"),
-    url(r"^curriculum/$", curriculum, name="curriculum"),
-    url(r"^curriculum/(?P<evento_ano>\d+)/$", curriculum_ano, name="curriculum_year"),
-    url(r"^equipa/$", equipa, name="team"),
-    url(r"^acervo/$", acervo, name="collection"),
-    url(r"^acervo/(?P<artist_id>\d+)/$", acervo_artist, name="collection_by_artist"),
-    url(
-        r"^acervo/(?P<artist_id>\d+)/(?P<obra_id>\d+)/$",
+    path("", index, name="gallery"),
+    path("galerias", galerias, name="galleries"),
+    path("missao/", missao, name="mission"),
+    path("historia/", historia, name="history"),
+    path("curriculum/", curriculum, name="curriculum"),
+    path("curriculum/<int:evento_ano>/", curriculum_ano, name="curriculum_year"),
+    path("equipa/", equipa, name="team"),
+    path("acervo/", acervo, name="collection"),
+    path("acervo/<int:artist_id>", acervo_artist, name="collection_by_artist"),
+    path(
+        "obras/<int:obra_id>/",
         acervo_detalhe,
         name="collection_detail",
     ),
-    url(r"^agenda/$", agenda, name="calendar"),
-    url(r"^premios/$", premios, name="awards"),
-    url(r"^premios/(?P<aniversario_id>\d+)/$", anniversary_detail, name="anniversary_detail"),
-    url(
-        r"^premios/(?P<aniversario_id>\d+)/premiados/$",
+    path("agenda/", agenda, name="calendar"),
+    path("premios/", premios, name="awards"),
+    path("premios/<int:aniversario_id>/", anniversary_detail, name="anniversary_detail"),
+    path(
+        "premios/<int:aniversario_id>/premiados/",
         premiados,
         name="anniversary_prizes",
     ),
-    url(r"^premios/(?P<aniversario_id>\d+)/trofeu$", trofeu, name="anniversary_trophy"),
-    url(r"^tela", tela, name="banner"),
+    path("premios/<int:aniversario_id>/trofeu", trofeu, name="anniversary_trophy"),
+    path("tela", tela, name="banner"),
 ]
